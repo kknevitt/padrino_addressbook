c55c6f7fb8c225f8193a95aed2d538f4817d74b4
kknevitt <knev123@hotmail.com>
2015-02-06 15:46:19 +0000

    added sign up and refactored routes to use REST

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -1,102 +1,99 @@
 require 'pry'
 require 'sinatra/flash'
 
-AddressBook::App.controllers :people do
+AddressBook::App.controllers :people, :map=>"person" do
 
  # edit path to use members only in protected paths
   before /.*\/[0-9]+/ do
     unless session[:logged_in] || request.path.start_with?('/people/login') || request.path.start_with?('/people/signup')
       flash[:notice] = "Not logged in"
-      redirect "/people/login"
+      redirect url_for(:people, :login)
     else
-      @person = Person.find(params[:id])
+      # need to filter members only for this to work.
+      # @person = Person.find(params[:id])
     end
   end
 
-  get '/edit_type_preferences/:id' do
+  get :index do
+    @people = Person.all
+    render "people/homepage"
+  end
+
+  get :new do
+  render "people/new"
+  end
+
+  post :create do
+    @person = Person.new(params[:person])
+    @person.save
+    redirect url_for(:people, :index)
+  end
+
+  delete :delete, :map => ":id" do
+    @person.destroy
+    redirect url_for(:people, :index)
+  end
+
+  get :edit, :map => ":id/edit" do
+    @person = Person.find(params[:id])
+    render "people/edit"
+  end
+
+  put :update, :map => ":id" do
+    @person = Person.find(params[:id])
+    person = params[:trainee] || params[:instructor]
+    @person.update(person)
+    redirect url_for(:people, :index)
+  end
+
+  get :role_preferences, :map => ":id/role_preferences" do
     render "people/preferences"
   end
 
-  post '/edit_type_preferences/:id' do
+  put :role_preferences, :map => ":id/" do
+    @person = Person.find(params[:id])
     @person.update(params[:person])
-    redirect 'people/all'
+    redirect url_for(:people, :index)
   end
 
-  get '/login' do
+  post :surname do
+    @people = Person.where('last_name LIKE ?', "#{params[:surname]}%");
+    render "people/search_results"
+  end
+
+  get :login, :map => "login" do
     render "people/login"
   end
 
-  get '/signup' do
-    render 'people/signup'
-  end
-
-  get '/members_only' do
-      "Secret Stuff"
-  end
-
-  post '/login' do
+  post :login do
     if User.where(:user_name=>params[:user_name]).exists?
       @user = User.find_by_user_name(params[:user_name])
       if @user.password == params[:password]
         flash[:notice] = "Welcome Back #{@user.user_name}"
         session[:logged_in] = true
-        redirect 'people/all'
+        redirect url_for(:people, :index)
       else
         flash[:notice] = "Password not recognised"
-        redirect 'people/login'
+        redirect url_for(:people, :login)
       end
     else
       flash[:notice] = "No such user"
-      redirect 'people/login'
+      redirect url_for(:people, :login)
     end
   end
 
-
-  get '/all' do
-    @people = Person.all
-    render "people/homepage"
+  get :sign_up do
+    render 'people/signup'
   end
 
-  get '/new' do
-    @person = Person.new(:first_name => "first name")
-  render "people/new"
+  post :sign_up do
+    @user = User.new(:user_name => params[:user_name], :password =>params[:password])
+    @user.save
+    redirect url_for(:people, :index)
   end
 
-  post '/create' do
-
-    @person = Person.new(params[:person])
-    @person.save
-
-    redirect "/people/all"
-
-  end
-
-  post '/surname/' do
-    # @all_people = Person.all
-    # @with_surname = @all_people.select do |person|
-    #  person.last_name.start_with? params[:letter]
-    # end
-
-    # refactor to put this functionality in Person model
-    # letter = "#{:letter}"
-  @people = Person.where('last_name LIKE ?', "#{params[:surname]}%");
-    render "people/search_results"
-  end
-
-  post '/delete/:id' do
-    @person.destroy
-    redirect 'people/all'
-  end
-
-  get '/edit/:id' do
-    render "people/edit"
-  end
-  post '/update/:id' do
-    @person.update(params[:person])
-    redirect 'people/all'
-  end
-
-  get '/:id' do
+  get :index, :with => :id do
+    @person = Person.find(params[:id])
     render "people/person"
   end
 
